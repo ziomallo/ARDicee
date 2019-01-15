ab9142299a78aede30fe33d681efe622c239683a
Andrzej Stojak <andrzej.stojak@gmail.com>
2019-01-15 14:51:06 +0100

    Dice model added to scene

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -22,28 +22,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.02)
-        let sphere = SCNSphere(radius: 0.2)
-        
-        let material = SCNMaterial()
-        material.diffuse.contents = MDLTexture(named: "art.scnassets/8k_moon.jpg")
-        
-        sphere.materials = [material]
-        
-        let node = SCNNode()
-        node.position = SCNVector3(0, -0.1, -0.3)
-        
-        node.geometry = sphere
-        
-        sceneView.scene.rootNode.addChildNode(node)
-        sceneView.autoenablesDefaultLighting = true
+
         
         // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+            
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        }
+        
 //        // Set the scene to the view
-//        sceneView.scene = scene
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
